eec69d52893bc4de0b0537d21f8b9227a041ee9f
SangBV <vsvn996@gmail.com>
2020-01-27 14:50:02 +0700

    Update Package

---
+++ Data/TodoListApp.Data.Common/Migrations/20200127074715_InitalDB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TodoListApp.Data.Common.Migrations
+{
+    public partial class InitalDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
