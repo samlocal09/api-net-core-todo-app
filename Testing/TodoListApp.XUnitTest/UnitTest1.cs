8ea13e7735198b143a4ddf7e027c4cd2ad2c6ab4
SangBV <vsvn996@gmail.com>
2020-02-16 23:42:28 +0700

    [Add] xUnit Test Project

---
+++ Testing/TodoListApp.XUnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Moq;
+using System;
+using TodoListApp.Data.Common.Repositories;
+using Xunit;
+
+namespace TodoListApp.XUnitTest
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+		}
+	}
+}
